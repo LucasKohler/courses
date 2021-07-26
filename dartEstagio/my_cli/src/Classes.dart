c8bfd9b869e965e0e4b21b9cfccca5c0af857052
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2021-07-26 18:08:43 -0300

    organizing folders

--- dartEstagio/my_cli/src/Classes.dart
+++ dartEstagio/my_cli/src/Classes.dart
@@ -6,6 +6,25 @@ class Professor {
   Professor(this._nome, this._matricula, this._idade);
 }
 
+class Ponto {
+  double _x;
+  double _y;
+
+  Ponto(this._x, this._y);
+
+  get coordenadaX => _x;
+
+  get coordenadaY => _y;
+
+  set novaCoordenadaX(double x) {
+    this._x = x;
+  }
+
+  set novaCoordenadaY(double y) {
+    this._y = y;
+  }
+}
+
 void main() {
   var p1 = Professor('girafales', 50, 100000);
   // p1.nome = 'Girafales;
