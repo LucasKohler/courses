4d396aed774c7e08fd7c5d7d0f4b23fa7e3af1d4
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2021-07-31 11:41:30 -0300

    rename directory and removed debug banner

--- flutterEstagio/startup_namer_part1/lib/main.dart
+++ flutterEstagio/startup_namer_part1/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'startup name generator',
       home: RandomWords(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
